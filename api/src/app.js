56dc0e6f7047a29603c740a655a78161f6439efa
Farzaamm <farzamkhonamri@gmail.com>
2025-09-08 05:39:37 +0000

    cors imported && prisma.js created

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 // const bcrypt = require("bcryptjs");
 // const jwt = require("jsonwebtoken");
 // const { PrismaClient } = require("@prisma/client");
 // const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
